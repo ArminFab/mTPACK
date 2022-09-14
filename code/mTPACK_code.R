e1b3678ae1f55aafdacb2207d457224c26c6041e
Armin Fabian <armin.fabian@uni-tuebingen.de>
2022-09-14 16:22:13 +0200

    removing those that did not finish sosci survey

--- code/mTPACK_code.R
+++ code/mTPACK_code.R
@@ -17,9 +17,19 @@ data_merged <- merge(data_sosci, data_coding_TPACK, by = "CASE")
 
 ################## data cleaning #############################
 
-# remove last row (as it depits headers)
+# remove last row (as it depicts headers)
 data_merged <- slice(data_merged, 1: (n()) - 1)
 
 
-# Gender characteristic -> factors ("1" = weiblich, "2" = "männlich", "3" = divers)
- 
+# Gender: String -> numeric (1=weiblich, 2=männlich, 3=divers)
+data_merged$GEN <- as.numeric(data_merged$GEN)
+
+# Removing SERIAL, REF as they have only missings
+
+data_merged <- data_merged[ , !names(data_merged) %in% c("SERIAL", "REF")]
+
+# Removing participants that did not finish the sosci survey
+
+data <- filter(data_merged, data_merged$FINISHED == "1")
+
+
