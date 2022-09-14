8804b52c522ed719918e327678ae0117a0dffbe8
Armin Fabian <armin.fabian@uni-tuebingen.de>
2022-09-14 11:26:51 +0200

    imported raw data

---
+++ code/mTPACK_code.R
@@ -0,0 +1,14 @@
+# libraries
+
+library(tidyverse)
+library(rio)
+
+# data import
+
+data_sosci <- rio::import("data_TPACK_Studie_2022-07-29_10-12.xlsx")  # raw-data from Sosci
+
+data_coding_TPACK <- rio::import("TPACK_Kodierung_ab1307.xlsx")    # raw-data from coding TPACK
+
+
+
+
