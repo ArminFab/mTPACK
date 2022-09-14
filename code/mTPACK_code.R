223db2836f1b1087d98fe0890777d29dc180e916
Armin Fabian <armin.fabian@uni-tuebingen.de>
2022-09-14 12:24:32 +0200

    deleting last row

--- code/mTPACK_code.R
+++ code/mTPACK_code.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(rio)
 library(dplyr)
 
+
 # data import
 
 data_sosci <- rio::import("data_TPACK_Studie_2022-07-29_10-12.xlsx")  # raw-data from Sosci
@@ -14,3 +15,11 @@ data_coding_TPACK <- rio::import("TPACK_Kodierung_ab1307.xlsx")    # raw-data fr
 
 data_merged <- merge(data_sosci, data_coding_TPACK, by = "CASE")
 
+################## data cleaning #############################
+
+# remove last row (as it depits headers)
+data_merged <- slice(data_merged, 1: (n()) - 1)
+
+
+# Gender characteristic -> factors ("1" = weiblich, "2" = "männlich", "3" = divers)
+ 
