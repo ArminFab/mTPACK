00399397424fabe5383d6d3abf45545670af8d65
Armin Fabian <armin.fabian@uni-tuebingen.de>
2022-09-16 14:07:23 +0200

    preparing variable TK
    first CFA with PCK, TPACK & TK

--- code/mTPACK_code.R
+++ code/mTPACK_code.R
@@ -31,8 +31,9 @@ data_merged$GEN <- as.numeric(data_merged$GEN)
 # Removing SERIAL, REF as they have  missings only
 data_merged <- data_merged[ , !names(data_merged) %in% c("SERIAL", "REF")]
 
-# Removing participants that did not finish the sosci survey
-data <- filter(data_merged, data_merged$FINISHED == "1")
+# Removing participants that did not finish the sosci survey OR gave consent
+data <- filter(data_merged, data_merged$FINISHED == "1" & data_merged$IN03 == "1")
+
 
 
 
@@ -57,10 +58,10 @@ data_TPACK <- select(data, TPACK_1_SUMME,
                            TPACK_8_SUMME)
 
 # Missing pattern visual
-print(vis_miss(data_TPACK))
+#print(vis_miss(data_TPACK))
 
 # correlations between TPACK items using FIML (library psych)
-print (corFiml(data_TPACK, covar = FALSE,show=FALSE))
+#print (corFiml(data_TPACK, covar = FALSE,show=FALSE))
 
 ###################### PCK ###############################################
 
@@ -92,8 +93,29 @@ print (corFiml(data_TPACK, covar = FALSE,show=FALSE))
 # PCK_12: Answer is correct if == 3
 # PCK_13: Answer is correct if == 14
 
-# Recoding each PCK_item to binary variables (1, if the correct answer was given, 0 otherwise)
+# Information on correct TK-answers
+# TK_spread_1 correct if ==1
+# TK_spread_2 correct if ==1
+# TK_spread_3 correct if ==4
+# TK_spread_4 correct if == 1 
+# TK_whiteboard_1 correct if == 2
+# TK_whiteboard_2 correct if == 1
+# TK_whiteboard_3 correct if == 3
+# TK_collab_1   correct if == 2
+# TK_collab_2   correct if == 1
+# TK_present_1  correct if = 3
+# TK_present_2    correct if == 2
+# TK_present_3 correct if == 4
+# TK_webbrowser correct if == 2
 
+
+
+
+
+
+
+
+# Recoding each PCK_item to binary variables (1, if the correct answer was given, 0 otherwise)
 data$PCK_1a <- ifelse(data$PCK_1a == "1",1,0) # If PCK_1a was answered correctly, assign value 1; otherwise 0
 data$PCK_1b <- ifelse(data$PCK_1b == "2",1,0) 
 data$PCK_1c <- ifelse(data$PCK_1c == "2",1,0) 
@@ -121,8 +143,37 @@ data$PCK_11d  <- ifelse(data$PCK_11d == "1",1,0)
 data$PCK_12   <- ifelse(data$PCK_12 == "3",1,0)
 data$PCK_13  <- ifelse(data$PCK_13 == "14",1,0)
 
-# df containing each 26 PCK items
+# Recoding each TK_Item to binary variables
+data$TK_spread_1 <- ifelse(data$TK_spread_1 == "1",1,0)
+data$TK_spread_2 <- ifelse(data$TK_spread_2 == "1",1,0)
+data$TK_spread_3 <- ifelse(data$TK_spread_3 == "4",1,0)
+data$TK_spread_4 <- ifelse(data$TK_spread_4 == "1",1,0)
+data$TK_whiteboard_1 <- ifelse(data$TK_whiteboard_1 == "2",1,0)
+data$TK_whiteboard_2 <- ifelse(data$TK_whiteboard_2 == "1",1,0)
+data$TK_whiteboard_3 <- ifelse(data$TK_whiteboard_3 == "3",1,0)
+data$TK_collab_1 <- ifelse(data$TK_collab_1 == "2",1,0)
+data$TK_collab_2 <- ifelse(data$TK_collab_2 == "1",1,0)
+data$TK_present_1 <- ifelse(data$TK_present_1 == "3",1,0)
+data$TK_present_2 <- ifelse(data$TK_present_2 == "2",1,0)
+data$TK_present_3 <- ifelse(data$TK_present_3 == "4",1,0)
+data$TK_webbrowser <- ifelse(data$TK_webbrowser == "2",1,0)
 
+# df data_TK consisting of each TK_ item
+data_TK <- select(data, TK_spread_1,    
+                        TK_spread_2,
+                        TK_spread_3,  
+                        TK_spread_4,  
+                        TK_whiteboard_1,
+                        TK_whiteboard_2,
+                        TK_whiteboard_3,
+                        TK_collab_1,  
+                        TK_collab_2,  
+                        TK_present_1, 
+                        TK_present_2, 
+                        TK_present_3, 
+                        TK_webbrowser)
+
+# df containing each 26 PCK items
 data_PCK <- select(data, PCK_1a, 
                          PCK_1b, 
                          PCK_1c, 
@@ -152,15 +203,17 @@ data_PCK <- select(data, PCK_1a,
 
 ######## CFA for TPACK & PCK ########
 
-# dataframe including PCK and TPACK variables
+# creating dataframe including knowledge constructs (PCK and TPACK and TK variables)
 data_TPACK <- mutate(data_TPACK, CASE= data$CASE)
 data_PCK <- mutate(data_PCK, CASE=data$CASE)
-data_TPACK_PCK <-left_join(data_TPACK, data_PCK, by = "CASE")
-
+data_TK <- mutate(data_TK, CASE=data$CASE)
+data_knwoledge <-left_join(data_TPACK, data_PCK, by = "CASE")
 data_list <- list(data_PCK, data_TPACK)
+
 # Measurement model
-mod1 <- 'TPACK=~TPACK_1_SUMME+TPACK_2_SUMME+TPACK_3_SUMME+TPACK_4_SUMME+TPACK_5_SUMME+TPACK_6_SUMME+TPACK_7_SUMME+TPACK_8_SUMME
-          PCK =~PCK_1a+PCK_1b+PCK_1c+PCK_2a+PCK_2b+PCK_2c+PCK_3+PCK_5a+PCK_5b+PCK_5c+PCK_6a+PCK_6b+PCK_7a+PCK_7b+PCK_8a+PCK_8b+PCK_9+PCK_10a+PCK_10b+PCK_10c+PCK_11a+PCK_11b+PCK_11c+PCK_11d+PCK_12+PCK_13'
+mod1 <- 'TPACK =~ TPACK_1_SUMME+TPACK_2_SUMME+TPACK_3_SUMME+TPACK_4_SUMME+TPACK_5_SUMME+TPACK_6_SUMME+TPACK_7_SUMME+TPACK_8_SUMME
+          PCK  =~ PCK_1a+PCK_1b+PCK_1c+PCK_2a+PCK_2b+PCK_2c+PCK_3+PCK_5a+PCK_5b+PCK_5c+PCK_6a+PCK_6b+PCK_7a+PCK_7b+PCK_8a+PCK_8b+PCK_9+PCK_10a+PCK_10b+PCK_10c+PCK_11a+PCK_11b+PCK_11c+PCK_11d+PCK_12+PCK_13
+          TK   =~ TK_spread_1  + TK_spread_2+TK_spread_3+TK_spread_4+TK_whiteboard_1+TK_whiteboard_2+TK_whiteboard_3+TK_collab_1+TK_collab_2+TK_present_1+TK_present_2+TK_present_3+TK_webbrowser'
 
 
 # fitting mod1
@@ -174,14 +227,6 @@ lavaanPlot(model = fit_mod1,
            node_options = list(shape = "box", fontname = "Helvetica"), 
            edge_options = list(color = "grey"), 
            coefs = T,covs = T, stars = T, stand = T)
-
-
-
-
-
-
-
-
 
 
 
